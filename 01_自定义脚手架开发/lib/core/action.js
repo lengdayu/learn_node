0a7cd9344853ee906861ed2d06cde887f669325f
葫芦娃 <78081228+lengdayu@users.noreply.github.com>
2023-04-11 23:27:56 +0800

    自定义脚手架开发

--- 01_自定义脚手架开发/lib/core/action.js
+++ 01_自定义脚手架开发/lib/core/action.js
@@ -1,24 +1,18 @@
 // npm install --save inquirer@^8.0.0
 // 9版本以上不知require导入，需要使用esm方式应用
-const inquirer = require("inquirer");
-const myAction = function (project, args) {
-  inquirer
-    .prompt([
-      //问题配置
-      {
-        type: "input",
-        name: "username",
-        message: "你的名字",
-      },
-      {
-        type: "list",
-        name: "framwork",
-        choices: ["express", "koa2", "egg"],
-        message: "请选择你所使用的框架",
-      },
-    ])
-    .then((answer) => {
-      console.log(answer);
-    });
+var inquirer = require("inquirer");
+var config = require("../../config");
+var downloadFun = require("./download");
+const myAction = async (project, args) => {
+  const answer = await inquirer.prompt([
+    {
+      type: "list",
+      name: "framwork",
+      choices: config.framwork,
+      message: "请选择你所使用的框架",
+    },
+  ]);
+  // 下载代码模板
+  downloadFun(config.foramworkUrl[answer.framwork], project);
 };
 module.exports = myAction;
