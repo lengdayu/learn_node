2d3a052b4aac6e2ccdce1ae5fd28da6daa1dd92a
葫芦娃 <78081228+lengdayu@users.noreply.github.com>
2023-06-16 19:03:34 +0800

    个人切换npm镜像源工具legndayu

--- 00_demo/lib/core/action.mjs
+++ 00_demo/lib/core/action.mjs
@@ -1,40 +1,45 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
+import { exec } from "child_process";
+
+const registryList = {
+  npm: "https://registry.npmjs.org",
+  cnpm: "http://r.cnpmjs.org",
+  taobao: "https://registry.npm.taobao.org",
+  nj: "https://registry.nodejitsu.com",
+  npmMirror: "https://skimdb.npmjs.com/registry",
+  edunpm: "http://registry.enpmjs.org",
+};
+
 const myAction = async (project, args) => {
-  console.log(chalk.green("开始配置："));
+  let choices = [];
+  for (const key in registryList) {
+    if (Object.hasOwnProperty.call(registryList, key)) {
+      choices.push(key);
+    }
+  }
   const answer = await inquirer.prompt([
     {
       type: "list",
       name: "choose",
-      choices: ["npm", "cnpm", "taobao", "nj", "npmMirror", "edunpm"],
+      choices,
       message: "请选择下面列表中的npm镜像地址!",
     },
   ]);
-  //   npm ---- https://registry.npmjs.org/
-  //   cnpm --- http://r.cnpmjs.org/
-  //   taobao - https://registry.npm.taobao.org/
-  //   nj ----- https://registry.nodejitsu.com/
-  //   npmMirror  https://skimdb.npmjs.com/registry/
-  //   edunpm - http://registry.enpmjs.org/
-  switch (answer.choose) {
-    case "npm":
-      console.log();
-      break;
-    case "cnpm":
-      break;
-    case "taobao":
-      break;
-    case "nj":
-      break;
-    case "npmMirror":
-      break;
-    case "edunpm":
-      break;
-    case "npm":
-      break;
-
-    default:
-      break;
-  }
+  changeR(answer.choose, registryList[answer.choose]);
 };
+
+const changeR = async (choose, registry) => {
+  console.log(chalk.green(`开始切换 ${choose} 镜像地址`));
+  await new Promise((resolve, reject) => {
+    exec(`npm config set registry ${registry}`, (error, stdout, stderr) => {
+      if (error) {
+        reject(console.error(chalk.red(err)));
+      }
+      resolve({ stdout, stderr });
+    });
+  });
+  console.log(chalk.green(`${choose} 镜像地址切换完成！`));
+};
+
 export default myAction;
