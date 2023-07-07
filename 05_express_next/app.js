678b897a6b9faeb0a69c767c6783baceeec481a5
葫芦娃 <78081228+lengdayu@users.noreply.github.com>
2023-07-07 17:31:06 +0800

    增加日志库

--- 05_express_next/app.js
+++ 05_express_next/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 
 // 加一个注释，用以说明，本项目代码可以任意定制更改
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+app.use(morgan("dev"));
 
 const PORT = process.env.PORT || 3000;
 
